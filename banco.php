783285b9ed15885b42a73744e8fdb864189cb292
João V. S. S <joaoviniciussouzasilva@gmail.com>
2021-09-10 14:59:05 -0300

    Update

--- banco.php
+++ banco.php
@@ -1,3 +1,6 @@
 <?php
 
-require 'src/Conta.php';
+require_once 'src/Conta.php';
+
+$primeiraConta = new Conta('123.456.789-10', 'Fulano da Silva');
+var_dump($primeiraConta);
--- src/Conta.php
+++ src/Conta.php
@@ -2,35 +2,78 @@
 
 class Conta
 {
-    public string $cpfTitular;
-    public string $nomeTitular;
-    public float $saldo = 0;
+    private string $cpfTitular;
+    private string $nomeTitular;
+    private float $saldo;
+    private static int $numeroDeContas = 0;
 
+    // Construtor
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->validarNomeTitular($nomeTitular);
+        $this->$nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+        self::$numeroDeContas++;
+    }
+
+    // Getters
+    public function recuperarCpfTitular(): string
+    {
+        return $this->cpfTitular;
+    }
+
+    public function recuperarNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function recuperarSaldo(): float
+    {
+        return $this->saldo;
+    }
+    public static function RecuperarNumerodeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
+
+    // Métodos
     public function sacar(float $valorAsacar): void
     {
         if ($valorAsacar < $this->saldo) {
             echo 'Saldo indisponível';
-        } else {
-            $this->saldo -= $valorAsacar;
+            return;
         }
+
+        $this->saldo -= $valorAsacar;
     }
 
     public function depositar(float $valorAdepositar): void
     {
         if ($valorAdepositar < 0) {
             echo 'Este valor é inválido. Necessita ser positivo';
-        } else {
-            $this->saldo += $valorAdepositar;
+            return;
         }
+
+        $this->saldo += $valorAdepositar;
     }
 
     public function transferir(float $valorAtranferir, Conta $contaDestino)
     {
         if ($valorAtranferir > $this->saldo) {
             echo 'Saldo indisponível';
-        } else {
-            $this->sacar($valorAtranferir);
-            $contaDestino->depositar($valorAtranferir);
+            return;
+        }
+
+        $this->sacar($valorAtranferir);
+        $contaDestino->depositar($valorAtranferir);
+    }
+
+    private function validarNomeTitular(string $nomeTitular): void
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo 'ERRO: O nome deverá ter no mínimo 5 carcteres';
+            exit();
         }
     }
 }
